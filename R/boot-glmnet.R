caa07bf864da175a106cdc32fcb916960d1e0412
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-08-07 17:07:39 -0500

    boot.glmnet: progress bar optional, returns a data.frame

--- R/boot-glmnet.R
+++ R/boot-glmnet.R
@@ -6,17 +6,18 @@
 #' @param lambda     Regularization parameter at which solutions are to be bootstrapped (by default, uses cross-valiation to find lambda)
 #' @param seed       Seed (optional)
 #' @param alpha      Error rate; 0.05 (default) corresponds to 95\% confidence interval
+#' @param bar        Print a progress bar?
 #'
 #' @example ex/boot.R
 
-boot.glmnet <- function(X, y, B=500, lambda, seed, alpha=0.05) {
+boot.glmnet <- function(X, y, B=500, lambda, seed, alpha=0.05, bar=TRUE) {
   p <- ncol(X)
   n <- nrow(X)
   if (missing(lambda)) {
     cvfit <- cv.glmnet(X, y)
     lambda <- cvfit$lambda.min
   }
-  pb <- txtProgressBar(0, B, style=3)
+  if (bar) pb <- txtProgressBar(0, B, style=3)
   beta_hat <- matrix(NA, B, p, dimnames=list(1:B, colnames(X)))
   if (!missing(seed)) set.seed(seed)
   for (i in 1:B) {
@@ -24,10 +25,11 @@ boot.glmnet <- function(X, y, B=500, lambda, seed, alpha=0.05) {
     fit.i <- glmnet(X[ind,], y[ind], nlambda=20, lambda.min=lambda)
     b <- coef(fit.i, s=lambda)
     beta_hat[i,] <- b[-1]
-    setTxtProgressBar(pb, i)
+    if (bar) setTxtProgressBar(pb, i)
   }
-  close(pb)
-  out <- t(apply(beta_hat, 2, quantile, probs=c(alpha/2, 1-alpha/2)))
+  if (bar) close(pb)
+  out <- as.data.frame(t(apply(beta_hat, 2, quantile, probs=c(alpha/2, 1-alpha/2))))
   rownames(out) <- rownames(b)[-1]
+  names(out) <- c('Lower', 'Upper')
   out
 }
