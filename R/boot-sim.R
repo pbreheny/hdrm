f27dfab91c8b4af6d8135b8e2ff9d1bc3710c1fc
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-08-07 17:11:04 -0500

    boot-sim running

--- R/boot-sim.R
+++ R/boot-sim.R
@@ -2,45 +2,37 @@
 #'
 #' Reproduces Example 9.3 from the book.  If you specify any options, your results may look different.
 #'
-#' @param N     Number of simulated realizations
-#' @param B     Number of bootstrap replications (for each data set)
-#' @param n     Sample size
+#' @param N          Number of simulated realizations
+#' @param B          Number of bootstrap replications (for each data set)
+#' @param n          Sample size
+#' @param p          Number of features
+#' @param a          Number of causal ('A') variables
+#' @param b          Number of correlated ('B') variables
+#' @param rho        Correlation between 'A' and 'B' variables
+#' @param noise      Correlation structure between features ('exchangeable' | 'autoregressive')
+#' @param rho.noise  Correlation parameter for noise variables
+#' @param seed       Random number seed for reproducibility
+#' @param ...        Further arguments to \code{\link{genDataABN}}
 #'
 #' @examples
 #' out <- Ex9.3()
+#' mean(out$cov)
+#' mean(out$cov[, which(out$varType=="A")])
+#' mean(out$cov[, which(out$varType=="B")])
+#' mean(out$cov[, which(out$varType=="N")])
 
-
-Ex9.3 <- function(N=100, B=100, n=100, p=60, n.corr=2, rho=0.5, seed=1) {
-  N=10; B=100; n=100; p=60; n.corr=2; rho=0.5; seed=1
-  pb <- txtProgressBar(0, N, style=3)
+Ex9.3 <- function(N=100, B=100, n=100, p=100, a=10, b=2, rho=0.5, noise='autoregressive', rho.noise=0.8, seed=1, ...) {
   set.seed(seed)
+  pb <- txtProgressBar(0, N, style=3)
+  cov <- matrix(NA, N, p, dimnames=list(1:N, 1:p))
   for (i in 1:N) {
-    Data <- genDataABC(n=n, p=p, b=n.corr, rho=rho)
+    Data <- genDataABN(n=n, p=p, a=a, b=b, rho=rho, noise=noise, rho.noise=rho.noise, ...)
     cvfit <- cv.glmnet(Data$X, Data$y)
-    BB <- matrix(NA, B, p, dimnames=list(1:N, colnames(Data$X)))
-    for (j in 1:B) {
-      ind <- sample(1:n, replace=TRUE)
-      fit.j <- glmnet(Data$X[ind,], Data$y[ind], lambda=cvfit$lambda)
-      BB[j,] <- coef(fit.j, s=cvfit$lambda.min)[-1]
-    }
+    res <- boot.glmnet(Data$X, Data$y, lambda=cvfit$lambda.min, B=B, bar=FALSE)
+    cov[i,] <- Data$beta >= res$Lower & Data$beta <= res$Upper
+    setTxtProgressBar(pb, i)
   }
-  setTxtProgressBar(pb, i)
-
-  #B <-
-  close(pb)
-  cvg <- numeric(p)
-  SE <- apply(B, 2, SE)
-
-  #apply(B, 1, function(x) res$)
-
-  S <- apply(SS, 2:3, mean)
-  colnames(S) <- ncvreg:::lamNames(fit$lambda)
-  q <- apply(Q, 2, mean)
-
-  l <- fit$lambda
-  col <- rep(rgb(0.6, 0.6, 0.6, 0.25), p)
-  col[res$varType=="A"] <- "red"
-  matplot(l, t(S), type="l", lty=1, xlim=rev(range(l)), col=col, lwd=2, las=1, bty="n",
-          xlab=expression(lambda), ylab="Stability")
-
+  colnames(cov) <- colnames(Data$X)
+  out <- list(cov=cov, varType=Data$varType)
+  out
 }
