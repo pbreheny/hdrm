788a6443701db5bee8c16a1f0a09c0636c05c854
Patrick Breheny <patrick-breheny@uiowa.edu>
2018-08-09 10:07:36 -0500

    boot-toy working; changed seed for resampling-simdata

--- R/boot-toy.R
+++ R/boot-toy.R
@@ -1,30 +1,30 @@
-#' Reproduce Figure 9.3
+#' Reproduce Figure
 #'
-#' Reproduces Figure 9.3 from the book.  If you specify any options, your results may look different.
+#' Reproduces Figure from the book.  If you specify any options, your results may look different.
 #'
 #' @param out   Output of Ex9.1
-#' @param N     Number of simulated realizations
+#' @param B     Number of bootstrap replications
 #' @param seed  Random number seed for reproducibility
 #'
 #' @examples
-#' out <- Ex9.1()
-#' res <- Fig9.3(out)
-#' covered <- out$beta >= res$Lower & out$beta <= res$Upper
+#' Data <- Ex9.1()
+#' res <- Fig(Data)
+#' covered <- Data$beta >= res$Lower & Data$beta <= res$Upper
 #' mean(covered)
-#' table(out$varType, covered)
+#' table(Data$varType, covered)
 
-Fig9.3 <- function(out, N=100, seed=1) {
+Fig <- function(out, B=100, seed=2) {
   set.seed(seed)
   cvfit <- cv.glmnet(out$X, out$y)
-  res <- boot.glmnet(out$X, out$y, lambda=cvfit$lambda.min)
-  BB <- cbind(coef(cvfit)[-1], res)
-  B <- BB[BB[,1] != 0,]
-  CIplot(B, sort=FALSE, xlab=expression(beta), xlim=c(-1.1, 1.1))
-  for (i in 1:nrow(B)) {
-    b <- out$beta[rownames(B)[i]]
-    line <- nrow(B) - i + 1
+  res <- boot.glmnet(out$X, out$y, lambda=cvfit$lambda.min, B=B)
+  all_coef <- cbind(coef(cvfit)[-1], res)
+  nz_coef <- all_coef[all_coef[,1] != 0,]
+  CIplot(nz_coef, sort=FALSE, xlab=expression(beta), xlim=c(-1.1, 1.1))
+  for (i in 1:nrow(nz_coef)) {
+    b <- out$beta[rownames(nz_coef)[i]]
+    line <- nrow(nz_coef) - i + 1
     lines(c(b,b), c(line-0.5, line+0.5), col="gray", lty=2, lwd=2, xpd=1)
   }
-  colnames(BB) <- c('Estimate', 'Lower', 'Upper')
-  return(invisible(as.data.frame(BB)))
+  colnames(all_coef) <- c('Estimate', 'Lower', 'Upper')
+  return(invisible(all_coef))
 }
--- R/resampling-simdata.R
+++ R/resampling-simdata.R
@@ -1,6 +1,6 @@
-#' Reproduce Example 9.1
+#' Reproduce Ex9.1 9.1
 #'
-#' Reproduces Example 9.1 from the book.  If you specify any options, your results may look different.
+#' Reproduces Ex9.1 9.1 from the book.  If you specify any options, your results may look different.
 #'
 #' @param n       Sample size
 #' @param p       Number of features
@@ -14,8 +14,7 @@
 #' @examples
 #' out <- Ex9.1()
 
-Ex9.1 <- function(n=100, p=60, a=6, b=2, rho=0.5, beta=c(1,-1,0.5,-0.5,0.5,-0.5), seed=1, ...) {
+Ex9.1 <- function(n=100, p=60, a=6, b=2, rho=0.5, beta=c(1,-1,0.5,-0.5,0.5,-0.5), seed=8, ...) {
   set.seed(seed)
-
   genDataABN(n=n, p=p, a=a, b=b, rho=rho, beta=beta, ...)
 }
