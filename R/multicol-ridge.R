3a8c3fc370a651130c39c7c38f8196030fe07eeb
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-02-06 14:44:09 -0600

    multicol-ridge: Added lambda option

--- R/multicol-ridge.R
+++ R/multicol-ridge.R
@@ -2,11 +2,14 @@
 #'
 #' Reproduces Example 1.2 from the book.
 #'
-#' @param seed   Random number seed for reproducibility
+#' @param seed     Random number seed for reproducibility
+#' @param lambda   Regularization parameter for ridge regression
 #'
-#' @examples Ex1.2()
+#' @examples
+#' Ex1.2()
+#' Ex1.2(lam=0.01)
 
-Ex1.2 <- function(seed=11) {
+Ex1.2 <- function(seed=11, lambda=0.1) {
   set.seed(seed)
   x1 <- rnorm(20)
   x2 <- rnorm(20, mean=x1, sd=.01)
@@ -14,5 +17,5 @@ Ex1.2 <- function(seed=11) {
   cat("lm:\n")
   print(coef(lm(y~x1+x2)))
   cat("ridge:\n")
-  coef(ridge(y~x1+x2), lambda=0.1)
+  coef(ridge(y~x1+x2), lambda=lambda)
 }
