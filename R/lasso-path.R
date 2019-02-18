cb0f94140053b43dc5fd23a82edb96a43c923ec2
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-02-18 15:12:24 -0600

    Fix missing .hdrm_data_sets issue

--- R/lasso-path.R
+++ R/lasso-path.R
@@ -2,6 +2,7 @@
 #'
 #' Reproduces Figure 2.5 from the book; if you specify any options, your results may look different.
 #'
+#' @param cv    Carry out cross-validation?  Default: true
 #' @param mar   Margins, passed to `par()`
 #'
 #' @examples
@@ -16,14 +17,18 @@
 #' cvfit <- cv.glmnet(X, y, nfolds=length(y))
 #' colnames(X)[unlist(predict(cvfit, type="nonzero"))]
 
-Fig2.5 <- function(mar=rep(5,4)) {
+Fig2.5 <- function(cv=TRUE, mar=rep(5,4)) {
   op <- par(mar=mar)
   Data <- readData('pollution')
   X <- std(Data$X)
   y <- Data$y
 
-  cvfit <- cv.glmnet(X, y)
-  fit <- cvfit$glmnet.fit
+  if (cv) {
+    cvfit <- cv.glmnet(X, y)
+    fit <- cvfit$glmnet.fit
+  } else {
+    fit <- glmnet(X, y)
+  }
   l <- fit$lambda
   b <- coef(fit)[-1,]
   nv <- sapply(predict(fit, type="nonzero"), length)
@@ -35,7 +40,7 @@ Fig2.5 <- function(mar=rep(5,4)) {
   ind <- abs(b[,ncol(b)]) > 15
   text(x=log(min(l)/2.5), y=b[which(ind),ncol(b)], colnames(X)[ind], xpd=TRUE)
   text(x=log(min(l)/2.5), y=10, "SO2", xpd=TRUE)
-  abline(v=log(cvfit$lambda.min), col="gray")
+  if (cv) abline(v=log(cvfit$lambda.min), col="gray")
   ind <- seq(1, length(l), length=5)
   axis(3, at=log(l)[ind], labels=round(nv[ind], 1))
   mtext("Number of nonzero coefficients", 3, 3)
--- R/readData.R
+++ R/readData.R
@@ -17,7 +17,7 @@
 
 downloadData <- function(name) {
   if (missing(name)) {
-    name <- .hdrm_data_sets
+    name <- hdrm_data_sets
   } else {
     name <- substitute(name)
   }
@@ -40,8 +40,9 @@ downloadData <- function(name) {
 #' head(Data$y)
 
 readData <- function(name) {
-  if (!(name %in% .hdrm_data_sets)) stop(paste0(name, ' is not an hdrm data set'))
-  FILE <- paste0(system.file("data", package="hdrm"), '/', substitute(name), '.rds')
+  name <- substitute(name)
+  if (!(name %in% hdrm_data_sets)) stop(paste0(name, ' is not an hdrm data set'))
+  FILE <- paste0(system.file("data", package="hdrm"), '/', name, '.rds')
   if (!file.exists(FILE)) stop("You have to run downloadData() first; see ?downloadData", call.=FALSE)
   readRDS(FILE)
 }
@@ -55,8 +56,9 @@ readData <- function(name) {
 #' head(y)
 
 attachData <- function(name, envir=parent.frame()) {
-  if (!(name %in% .hdrm_data_sets)) stop(paste0(name, ' is not an hdrm data set'))
-  FILE <- paste0(system.file("data", package="hdrm"), '/', substitute(name), '.rds')
+  name <- substitute(name)
+  if (!(name %in% hdrm_data_sets)) stop(paste0(name, ' is not an hdrm data set'))
+  FILE <- paste0(system.file("data", package="hdrm"), '/', name, '.rds')
   if (!file.exists(FILE)) stop("You have to run downloadData() first; see ?downloadData", call.=FALSE)
   Data <- readRDS(FILE)
   for (x in names(Data)) {
