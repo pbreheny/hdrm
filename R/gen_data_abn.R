61feb1e6ac563e939bb4d9122f21a17b344bbb68
Logan Harris <lharris4211998@gmail.com>
2023-12-07 13:49:01 -0600

    Update documentation

--- R/gen_data_abn.R
+++ R/gen_data_abn.R
@@ -1,4 +1,4 @@
-#' Simulate data for regression models
+#' Simulate data according to Causal/Correlated/Noise paradigm
 #'
 #' This function is designed to scale efficiently to high dimensions, and
 #' therefore imposes some restrictions. For example, correlation must be
@@ -8,18 +8,38 @@
 #' determine an appropriate coefficient size. This will be slow if the dimension
 #' is large and beta is not sparse.
 #'
-#' @param n      Sample size
-#' @param p      Number of features
-#' @param p1     Number of nonzero features
-#' @param beta   Vector of regression coefficients in the generating model, or, if a scalar, the value of each nonzero regression coefficient.
-#' @param family Generate \code{y} according to linear "gaussian" or logistic "binomial" model
-#' @param SNR    Signal to noise ratio
-#' @param signal Should the beta coefficients be homogeneous (default) or heterogeneous
-#' @param corr   Correlation structure between features ('exchangeable' | 'autoregressive')
-#' @param rho    Correlation coefficient
+#' @param n          Sample size
+#' @param p          Number of features
+#' @param a          Number of causal ('A') variables
+#' @param b          Number of correlated ('B') variables per causal ('A') variable
+#' @param rho        Correlation between 'A' and 'B' variables
+#' @param family     Generate \code{y} according to linear "gaussian" or logistic "binomial" model
+#' @param signal     Should the groups be heterogeneous (in beta) or homogeneous?
+#' @param noise      Correlation structure between features ('exchangeable' | 'autoregressive')
+#' @param rho.noise  Correlation parameter for noise variables
+#' @param beta       Vector of regression coefficients in the generating model.  Should be either a scalar, in which case it represents the value of each nonzero regression coefficient, or a vector, in which case it should be of length \code{a}
+#' @param SNR        Signal to noise ratio
 #'
 #' @examples
+#' Data <- gen_data_abn(n=100, p=20, a=2, b=3)
+#' expect_equal(dim(Data$X), c(100, 20))
+#' expect_equal(length(Data$y), 100)
+#' expect_equal(Data$varType[1:8], rep(c('A', 'B', 'B', 'B'), 2))
+#' with(Data, data.frame(beta, varType))
+#
+#' gen_data_abn(100, 10, 2, 1)$beta
+#' gen_data_abn(100, 10, 2, 1, rho=0.9)$beta
+#' gen_data_abn(100, 10, 2, 1, rho=0.9, rho.noise=0.9)$beta
+#' gen_data_abn(100, 10, 2, 1, SNR=3)$beta
+#' gen_data_abn(100, 10, 2, 1, SNR=3, signal='het')$beta
+#' gen_data_abn(100, 10, 2, 1, beta=3)$beta
+#' gen_data_abn(100, 10, 2, 1, beta=2:1)$beta
 #'
+#' gen_data_abn(10, 20, 2, 3, family='binomial')$y
+#'
+#' gen_data_abn(1000, 10, 2, 2, rho=0.25, rho.noise=0.0, noise='exch')$X |> cor() |> round(digits=2)
+#' gen_data_abn(1000, 10, 2, 2, rho=0.5, rho.noise=0.5, noise='exch')$X |> cor() |> round(digits=2)
+#' gen_data_abn(1000, 10, 2, 2, rho=0.75, rho.noise=0.9, noise='auto')$X |> cor() |> round(digits=2)
 #' @export
 
 gen_data_abn <- function(n=100, p=60, a=6, b=2, rho=0.5, family=c("gaussian", "binomial"), signal=c('homogeneous', 'heterogeneous'), noise=c('exchangeable', 'autoregressive'),
--- R/gen_data_grp.R
+++ R/gen_data_grp.R
@@ -1,5 +1,13 @@
 #' Simulate grouped data for regression models
 #'
+#' This function is designed to scale efficiently to high dimensions, and
+#' therefore imposes some restrictions. For example, correlation must be
+#' positive.
+#'
+#' Note that if beta is not supplied, this function must calculate the SNR to
+#' determine an appropriate coefficient size. This will be slow if the dimension
+#' is large and beta is not sparse.
+#'
 #' @param n         Sample size
 #' @param J         Number of groups
 #' @param K         Number of features per group
@@ -13,8 +21,27 @@
 #' @param rho       Correlation between groups
 #' @param rho.g     Correlation between parameters within a group
 #'
-#' @example ex/genDataGrp.R
+#' @examples
+#' Data <- gen_data_grp(100, 10, 5, J1=3, K1=2)
+#' expect_equal(dim(Data$X), c(100, 50))
+#' head(Data$y)
+#' B <- matrix(Data$beta, ncol=10)
+#' expect_false(any(B[1:2, 1:3]==0))
+#' expect_true(all(B[3:5, 1:3]==0))
+#' expect_true(all(B[, 4:10]==0))
+#' expect_equal(Data$group, rep(1:10, each=5))
 #'
+#' gen_data_grp(100, 3, 3, J1=2, K1=2)$beta
+#' gen_data_grp(100, 3, 3, J1=2, K1=2, SNR=2)$beta
+#' gen_data_grp(100, 3, 3, J1=2, K1=2, SNR=2, rho=0.8)$beta
+#' gen_data_grp(100, 3, 3, J1=2, K1=2, SNR=2, rho=0.8, signal='het')$beta
+#' gen_data_grp(100, 3, 3, J1=2, K1=2, SNR=2, rho=0.8, signal='het', signal.g='het')$beta
+#' gen_data_grp(100, 3, 3, J1=2, K1=2, SNR=2, rho=0.8, signal='het', b=1)$beta
+#'
+#' gen_data_grp(1000, 3, 3, rho=0)$X |> cor() |> round(digits=2)
+#' gen_data_grp(1000, 3, 3, rho=0.7)$X |> cor() |> round(digits=2)
+#' gen_data_grp(1000, 3, 3, rho=0.3, rho.g=0.8)$X |> cor() |> round(digits=2)
+#' gen_data_grp(1000, 3, 3, rho=0.1, rho.g=0.5)$X |> cor() |> round(digits=2)
 #' @export
 
 gen_data_grp <- function(n, J, K=1, beta, family=c("gaussian","binomial"), J1=ceiling(J/2), K1=K, SNR=1, signal = c("homogeneous","heterogeneous"),
