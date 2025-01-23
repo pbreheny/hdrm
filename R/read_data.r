d981ca5f6daf37a57b51c1501b200ed60e11de82
Patrick Breheny <patrick-breheny@uiowa.edu>
2025-01-23 10:19:07 -0600

    revamping read_data(); fixes #7

--- R/read_data.r
+++ R/read_data.r
@@ -4,23 +4,21 @@
 #' To keep the size of the `hdrm` package manageable, these data sets are not included with the package and must be downloaded separately using `downloadData()`; this only needs to be done once.
 #'
 #' Data sets in hdrm are lists consisting of `X`, `y`, and possibly other elements.
-#' `readData` returns the list as an object that the user can name and use how they want.
-#' `attachData` assigns the elements of the list into the user's environment.
-#' `attachData` is often more convenient, but offers less control and potentially introduces side effects, such as overwriting an existing object `X` if one already exists.  See the examples for more details.
+#' `read_data` returns the list as an object that the user can name and use how they want.
+#' `attach_data` assigns the elements of the list into the user's environment.
+#' `attach_data` is often more convenient, but offers less control and potentially introduces side effects, such as overwriting an existing object `X` if one already exists.  See the examples for more details.
 #'
 #' @param name    Name of data set to read/attach
 #'
 #' @examples
 #' \dontrun{
-#' downloadData()         # Download all data sets
-#' downloadData(brca1)    # Download a specific data set}
+#' download_data()         # Download all data sets
+#' download_data(brca1)    # Download a specific data set}
 #' @export
 
-downloadData <- function(name) {
+download_data <- function(name) {
   if (missing(name)) {
     name <- hdrm_data_sets
-  } else {
-    name <- as.character(substitute(name))
   }
   for (id in name) {
     ext <- if (id == 'glioma') '.txt' else '.rds'
@@ -28,48 +26,47 @@ downloadData <- function(name) {
       'https://github.com/IowaBiostat/data-sets/raw/main/',
       id, '/', id, ext)
     FILE <- paste0(system.file("extdata", package="hdrm"), '/', id, '.rds')
-    download.file(URL, FILE, mode='wb')
+    try(suppressMessages(suppressWarnings(download.file(URL, FILE, mode = 'wb'))), silent = TRUE)
+    if (!file.exists(FILE)) {
+      stop(paste0("Unable to download data set '", name, "'"), call. = FALSE)
+    }
   }
 }
 
-#' @rdname downloadData
+#' @rdname download_data
 #'
 #' @param envir   If attaching, to which environment?
 #'
 #' @examples
-#'
-#' Data <- readData(brca1)
+#' Data <- read_data(brca1)
 #' dim(Data$X)
 #' head(Data$y)
-#'
 #' @export
 
-readData <- function(name) {
+read_data <- function(name) {
   name <- as.character(substitute(name))
   if (!(name %in% hdrm_data_sets)) stop(paste0(name, ' is not an hdrm data set'))
   FILE <- paste0(system.file("extdata", package="hdrm"), '/', name, '.rds')
-  if (!file.exists(FILE)) stop("You have to run downloadData() first; see ?downloadData", call.=FALSE)
+  if (!file.exists(FILE)) download_data(name)
   readRDS(FILE)
 }
 
-#' @rdname downloadData
+#' @rdname download_data
 #'
 #' @examples
-#'
-#' attachData(bcTCGA)
+#' attach_data(brca1)
 #' dim(X)
 #' head(y)
-#'
 #' @export
 
-attachData <- function(name, envir=parent.frame()) {
+attach_data <- function(name, envir=parent.frame()) {
   name <- as.character(substitute(name))
   if (!(name %in% hdrm_data_sets)) stop(paste0(name, ' is not an hdrm data set'))
   FILE <- paste0(system.file("extdata", package="hdrm"), '/', name, '.rds')
-  if (!file.exists(FILE)) stop("You have to run downloadData() first; see ?downloadData", call.=FALSE)
+  if (!file.exists(FILE)) download_data(name)
   Data <- readRDS(FILE)
   if (!is.list(Data)) {
-    stop(paste0("attachData() does not work for ", name, "; use readData() instead."))
+    stop(paste0("attach_data() does not work for ", name, "; use read_data() instead."))
   }
   for (x in names(Data)) {
     assign(x, Data[[x]], envir=envir)
