4a715dff738ffee07f00789dc3159e3cd3e93ac0
Logan Harris <lharris4211998@gmail.com>
2025-02-11 08:43:56 -0600

    Updating test that previously was for boot.glmnet

--- inst/tinytest/boot.r
+++ inst/tinytest/boot.r
@@ -1,4 +1,4 @@
 Data <- gen_data_abn(n=100, p=20, a=2, b=3)
-CI <- boot.glmnet(Data$X, Data$y)
-covered <- Data$beta >= CI[,1] & Data$beta <= CI[,2]
+CI <- boot_ncvreg(Data$X, Data$y)$confidence_intervals
+covered <- Data$beta >= CI[,"lower"] & Data$beta <= CI[,"upper"]
 table(covered)
